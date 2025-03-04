c8c283448f63fc262e24a402775c671a12f6bbc4
joniys <132161817+joniys@users.noreply.github.com>
2025-03-04 20:44:55 +0100

    lesson1

---
+++ lesson1/index.php
@@ -0,0 +1,17 @@
+<?php
+    $school = "dasasdasdsdaasd";
+    $nr1 = 120;
+    $nr2 = 50;
+
+    echo $nr1 + $nr2;
+    echo "<br>";
+
+    echo $nr1 * $nr2;
+    echo "<br>";
+
+    echo $nr1 / $nr2;
+    echo "<br>";
+
+    echo $nr1 % $nr2;
+    echo "<br>";
+?>
